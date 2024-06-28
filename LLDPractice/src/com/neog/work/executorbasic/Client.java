d71769f3b0eb7a1e9345b5bfcac1d4e381f83bde
govind2517 <prajapatigovind724@gmail.com>
2024-06-28 12:24:47 +0530

    Added ExecutorService so that after all threads runs, we can shutdown the executors service

--- LLDPractice/src/com/neog/work/executorbasic/Client.java
+++ LLDPractice/src/com/neog/work/executorbasic/Client.java
@@ -1,14 +1,15 @@
 package com.neog.work.executorbasic;
 
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Client {
     public static void main(String[] args) {
-        Executor executor = Executors.newFixedThreadPool(10);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
         for(int i=0; i<100; i++){
             NumberPrinter numberPrinter = new NumberPrinter(i);
-            executor.execute(numberPrinter);
+            executorService.execute(numberPrinter);
         }
+        executorService.shutdown();
     }
 }
