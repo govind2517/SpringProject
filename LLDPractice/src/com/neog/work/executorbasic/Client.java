833645afe3eaa0b968f24102fef06d7a13900084
govind2517 <prajapatigovind724@gmail.com>
2024-06-28 12:18:38 +0530

    After adding executor implementation only work done using 10 threads from pool

--- LLDPractice/src/com/neog/work/executorbasic/Client.java
+++ LLDPractice/src/com/neog/work/executorbasic/Client.java
@@ -1,10 +1,14 @@
 package com.neog.work.executorbasic;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class Client {
     public static void main(String[] args) {
+        Executor executor = Executors.newFixedThreadPool(10);
         for(int i=0; i<100; i++){
-            Thread t = new Thread(new NumberPrinter(i));
-            t.start();
+            NumberPrinter numberPrinter = new NumberPrinter(i);
+            executor.execute(numberPrinter);
         }
     }
 }
