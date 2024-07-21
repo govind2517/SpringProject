04f7757ce6136dea3bb3cc2b5a9e6c29661e4a9b
govind2517 <prajapatigovind724@gmail.com>
2024-07-21 22:28:57 +0530

    Actual implementatio of Builder Design Pattern

--- LLDPractice/src/com/neog/work/designpatterns/builder/Client.java
+++ LLDPractice/src/com/neog/work/designpatterns/builder/Client.java
@@ -2,16 +2,15 @@ package com.neog.work.designpatterns.builder;
 
 public class Client {
     public static void main(String[] args) {
-        //Builder b = new Builder();
-        Builder b = Student.getBuilder();
-        b.setName("Govind");
-        b.setAge(18);
-        b.setBatch("MorningBatch-Aug22");
-        b.setPsp(90);
-        b.setPhoneNumber("9845781251");
-        b.setUniversityName("Mumbai University");
-        //Student s = new Student(b);
-        Student s = b.build();
-        System.out.println(s);
+
+        Student student = Student.getBuilder()
+                        .setName("Govind")
+                        .setAge(22)
+                        .setPsp(80)
+                        .setPhoneNumber("9878451256")
+                        .build();
+
+        System.out.println("student "+student);
+        System.out.println("Completed...");
     }
 }
--- LLDPractice/src/com/neog/work/designpatterns/builder/Student.java
+++ LLDPractice/src/com/neog/work/designpatterns/builder/Student.java
@@ -12,20 +12,87 @@ public class Student {
         return new Builder();
     }
 
-    public Student(Builder builder){
-
-        if(builder.age < 18){
-            throw new RuntimeException("Student age cannot be less than 18");
-        }
-
-        if(builder.phoneNumber.length() < 10){
-            throw new RuntimeException("Student phone number cannot be less than 10");
-        }
+    private Student(Builder builder){
         this.name = builder.getName();
         this.age = builder.getAge();
         this.batch = builder.getBatch();
         this.universityName = builder.getUniversityName();
         this.psp = builder.getPsp();
         this.phoneNumber = builder.getPhoneNumber();
+    }
+
+    // inner class
+    public static class Builder {
+        String name;
+        int age;
+        String batch;
+        String universityName;
+        double psp;
+        String phoneNumber;
+
+        public String getName() {
+            return name;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public String getBatch() {
+            return batch;
+        }
+
+        public Builder setBatch(String batch) {
+            this.batch = batch;
+            return this;
+        }
+
+        public String getUniversityName() {
+            return universityName;
+        }
+
+        public Builder setUniversityName(String universityName) {
+            this.universityName = universityName;
+            return this;
+        }
+
+        public double getPsp() {
+            return psp;
+        }
+
+        public Builder setPsp(double psp) {
+            this.psp = psp;
+            return this;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public Builder setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Student build(){
+            if(this.age < 18){
+                throw new RuntimeException("Student age cannot be less than 18");
+            }
+            if(this.phoneNumber.length() < 10){
+                throw new RuntimeException("Student phone number cannot be less than 10");
+            }
+            // if all validation is passed then only create object
+            return new Student(this);
+        }
     }
 }
